59fee5dce2de84d64377bd99210e00d086b60e57
madeofhuman <josephodina@gmail.com>
2021-05-19 13:16:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serverless URL Shortener
+
+A basic URL shortener service built on serverless architecture. Capstone project for the Udacity Cloud Developer Nanodegree course.
---
+++ backend/serverless.ts
@@ -0,0 +1,81 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless-url-shortener',
+  configValidationMode: 'error',
+  frameworkVersion: '2',
+  variablesResolutionMode: '20210326',
+  disabledDeprecations: [
+    'CLI_OPTIONS_SCHEMA',
+    'UNSUPPORTED_CLI_OPTIONS'
+  ],
+  plugins: [
+    'serverless-iam-roles-per-function',
+    'serverless-offline',
+    'serverless-dynamodb-local',
+    'serverless-dotenv-plugin',
+    'serverless-bundle',
+  ],
+  package: {
+    individually: true
+  },
+  custom: {
+    ['serverless-offline']: {
+      httpPort: 3003,
+      babelOptions: {
+        presets: ["env"]
+      }
+    },
+    dynamodb: {
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+      stages: ["${self:custom.stage}"]
+    },
+    stage: "${opt:stage, 'dev'}",
+    region: "${opt:region, 'eu-west-2'}",
+  },
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    lambdaHashingVersion: '20201221',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    tracing: {
+      lambda: true,
+      apiGateway: true,
+    }
+  },
+  functions: {
+    hello
+  },
+  resources: {
+    Resources: {
+      GatewayResponseDefault4XX: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            ['gatewayresponse.header.Access-Control-Allow-Origin']: "'*'",
+            ['gatewayresponse.header.Access-Control-Allow-Headers']: "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            ['gatewayresponse.header.Access-Control-Allow-Methods']: "'GET,OPTIONS,POST'",
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: "ApiGatewayRestApi"
+          }
+        }
+      }
+    }
+  }
+};
+
+module.exports = serverlessConfiguration;
